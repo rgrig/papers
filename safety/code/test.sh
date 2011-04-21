2c01059fe553b27ad86e95bc1b5c8f951085dc5c
Radu Grigore <radugrigore@gmail.com>
2011-04-21 17:59:56 +0100

    Nicer "make examples"

--- safety/code/test.sh
+++ safety/code/test.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-for f in examples/*.j; do
-  echo "==$f"
-  rm -f out
-  touch out
-  for (( i = 0; i < 20; ++i )); do
-    ./gen.byte $i | ./interp.byte $f 2>> out
-  done
-  sort -u out
+#!/bin/sh
+rm -f .allout
+for (( i=0; i<20; ++i )); do
+  { ./gen.byte $i | OCAMLRUNPARAM=b ./interp.byte examples/*.j 2> .out; } || { cat .out; echo "seed $i"; exit 1; }
+  cat .out >> .allout
 done
+sort -u .allout
