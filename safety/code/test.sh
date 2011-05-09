9610465b342d556b756adbe7ce87229c02e220af
Radu Grigore <radugrigore@gmail.com>
2011-05-09 16:53:01 +0100

    Forced bash in test.sh.
    The for loop that works on Mac doe not work with dash (the POSIX-compliant
    shell on Ubuntu), so bash must be requested explicitly.

--- safety/code/test.sh
+++ safety/code/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 rm -f .allout
 for (( i=0; i<20; ++i )); do
   { ./gen.byte $i | OCAMLRUNPARAM=b ./interp.byte examples/*.j 2> .out; } || { cat .out; echo "seed $i"; exit 1; }
