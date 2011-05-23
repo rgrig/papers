c14c6047795ba34a70b919deb91de44a693f4d73
Radu Grigore <radugrigore@gmail.com>
2011-05-23 13:26:23 +0100

    Do not open Util from Tc.

--- safety/code/tc.ml
+++ safety/code/tc.ml
@@ -6,7 +6,7 @@
 (* modules *) (* {{{ *)
 open Ast
 open Format
-open Util
+module U = Util
 
 (* }}} *)
 (* environment and other utilities *) (* {{{ *)
@@ -28,10 +28,10 @@ module type EnvironmentT = sig
 end
 
 module Environment : EnvironmentT = struct (* {{{ *)
-  type t = 
-    { variables : type_ StringMap.t
-    ; fields_by_class : type_ StringMap.t StringMap.t
-    ; methods_by_class : (type_ * type_ list) StringMap.t StringMap.t 
+  type t =
+    { variables : type_ U.StringMap.t
+    ; fields_by_class : type_ U.StringMap.t U.StringMap.t
+    ; methods_by_class : (type_ * type_ list) U.StringMap.t U.StringMap.t
     ; line : int option }
 
   let object_layout env = env.fields_by_class
@@ -46,26 +46,26 @@ module Environment : EnvironmentT = struct (* {{{ *)
     | Bool | Unit -> ()
     | AnyType -> failwith "AnyType should not be created by parser."
     | Class c ->
-        if not (StringMap.mem c env.fields_by_class) then
+        if not (U.StringMap.mem c env.fields_by_class) then
           err env "class not declared" c
 
   let check_types_exist env =
     let check_type = check_type env in
     let check_method_type (r, args) =
       check_type r; List.iter check_type args in
-    let map_check check vs = StringMap.iter (fun _ t -> check t) vs in
+    let map_check check vs = U.StringMap.iter (fun _ t -> check t) vs in
     map_check check_type env.variables;
     map_check (map_check check_type) env.fields_by_class;
     map_check (map_check check_method_type) env.methods_by_class
 
   let add_var vs { declaration_variable = v; declaration_type = t } =
-    StringMap.add v t vs
+    U.StringMap.add v t vs
 
   let add_vars = List.fold_left add_var
 
   let process_member (fs, ms) = function
     | Field d -> (add_var fs d, ms)
-    | Ast.Method 
+    | Ast.Method
         { method_return_type = r
         ; method_name = n
         ; method_formals = args
@@ -73,20 +73,20 @@ module Environment : EnvironmentT = struct (* {{{ *)
       ->
         let gt x = x.declaration_type in
         let args = List.map gt args in
-        (fs, StringMap.add n (r, args) ms)
+        (fs, U.StringMap.add n (r, args) ms)
 
   let process_class (fbc, mbc) (cn, d) =
-    let fs, ms = StringMap.empty, StringMap.empty in
+    let fs, ms = U.StringMap.empty, U.StringMap.empty in
     let fs, ms = List.fold_left process_member (fs, ms) d in
-    (StringMap.add cn fs fbc, StringMap.add cn ms mbc)
+    (U.StringMap.add cn fs fbc, U.StringMap.add cn ms mbc)
 
   let make { program_globals=gs; program_classes=cs; program_main=_ } =
-    let fbc, mbc = StringMap.empty, StringMap.empty in
+    let fbc, mbc = U.StringMap.empty, U.StringMap.empty in
     let fbc, mbc = List.fold_left process_class (fbc, mbc) cs in
-    let env = 
-      { variables = add_vars StringMap.empty gs
+    let env =
+      { variables = add_vars U.StringMap.empty gs
       ; fields_by_class = fbc
-      ; methods_by_class = mbc 
+      ; methods_by_class = mbc
       ; line = None } in
     check_types_exist env;
     env
@@ -98,31 +98,31 @@ module Environment : EnvironmentT = struct (* {{{ *)
     { env with variables = add_vars env.variables d }
 
   let lookup_variable env id =
-    try StringMap.find id env.variables
+    try U.StringMap.find id env.variables
     with Not_found -> err env "undefined" id
 
   let get_class_info e m t =
-    let n = match t with 
-      | Class n -> n 
+    let n = match t with
+      | Class n -> n
       | _ -> err e "not a class" "?" in
-    try StringMap.find n m
+    try U.StringMap.find n m
     with Not_found -> err e "undefined class" n
 
   let lookup_method env t m =
     let ms = get_class_info env env.methods_by_class t in
-    try StringMap.find m ms
+    try U.StringMap.find m ms
     with Not_found -> err env "method not found" m
 
   let lookup_field env t f =
     let fs = get_class_info env env.fields_by_class t in
-    try StringMap.find f fs
+    try U.StringMap.find f fs
     with Not_found -> err env "field not found" f
 end (* }}} *)
 
 let check_types_match env t1 t2 =
   if t1 <> t2 && t1 <> AnyType && t2 <> AnyType then
     let p = Environment.position env in
-    let info = 
+    let info =
       fprintf str_formatter "@[%a and %a@]"
           pp_type t1 pp_type t2; flush_str_formatter () in
     fail p "type mismatch" info
@@ -135,14 +135,14 @@ let rec call env c =
   let string_of_class = function
     | Class c -> c
     | _ -> fail (Environment.position env) "expected class, not primitive" "" in
-  let tr = expression c.call_receiver in 
+  let tr = expression c.call_receiver in
   let tmr, tma = Environment.lookup_method env tr c.call_method in
   let ta = List.map expression c.call_arguments in
   c.call_class <- Some (string_of_class tr);
   (try List.iter2 check_types_match tma ta
   with Invalid_argument _ ->
     fail (Environment.position env) "wrong number of arguments" c.call_method);
-  (match c.call_lhs with 
+  (match c.call_lhs with
     | Some l -> check_types_match tmr (expression (Ref l))
     | _ -> ());
   Unit
@@ -177,7 +177,7 @@ and allocate env a =
   let t = expression (Ref a.allocate_lhs) in
   a.allocate_type <- Some t; Unit
 
-and statement env {ast = ast; line = line} = 
+and statement env {ast = ast; line = line} =
   let env = Environment.update_line env line in
   let allocate = allocate env in
   let body = body env in
@@ -186,12 +186,12 @@ and statement env {ast = ast; line = line} =
   let expression = expression env in
   match ast with
     | Return e -> expression e
-    | Assignment (s, e) -> 
+    | Assignment (s, e) ->
         check_types_match (expression (Ref s)) (expression e); Unit
     | Call c -> call c
     | Allocate a -> allocate a
     | While w -> while_ env w
-    | If (e, b) -> 
+    | If (e, b) ->
         check_types_match (expression e) Bool; body b
 
 and body env (Body (d, s)) =
@@ -228,7 +228,7 @@ module PropertyChecks = struct
   let set_location = function
     | None -> location := "?"
     | Some l -> location := sprintf "%d" l
-  let warn m = 
+  let warn m =
     warnings := sprintf "%s: Warning: %s" !location m :: !warnings
 
   let get_source e = e.A.edge_source
@@ -239,15 +239,15 @@ module PropertyChecks = struct
   let adjacency_of_edges source target es =
     let f acc e =
       let s, t = source e, target e in
-      let old = try StringMap.find s acc with Not_found -> [] in
-      StringMap.add s (t :: old) acc in
-    default [] StringMap.find (List.fold_left f StringMap.empty es)
+      let old = try U.StringMap.find s acc with Not_found -> [] in
+      U.StringMap.add s (t :: old) acc in
+    default [] U.StringMap.find (List.fold_left f U.StringMap.empty es)
 
   let rec reachable_from g s =
-    let r = ref StringSet.empty in
+    let r = ref U.StringSet.empty in
     let rec f s =
-      if not (StringSet.mem s !r) then begin
-        r := StringSet.add s !r;
+      if not (U.StringSet.mem s !r) then begin
+        r := U.StringSet.add s !r;
         List.iter f (g s)
       end in
     f s; !r
@@ -257,42 +257,42 @@ module PropertyChecks = struct
     let pred = adjacency_of_edges get_target get_source p.A.edges in
     let fs = reachable_from succ "start" in
     let te = reachable_from pred "error" in
-    let collect get s = 
-      List.fold_left (fun s e -> StringSet.add (get e) s) s p.A.edges in
-    let all = collect get_target (collect get_source StringSet.empty) in
-    let bad = StringSet.diff all (StringSet.inter fs te) in
-    if not (StringSet.mem "start" all) then warn "missing start";
-    if not (StringSet.mem "error" all) then warn "missing error";
-    StringSet.iter (fun s -> warn (sprintf "unused state: %s" s)) bad
+    let collect get s =
+      List.fold_left (fun s e -> U.StringSet.add (get e) s) s p.A.edges in
+    let all = collect get_target (collect get_source U.StringSet.empty) in
+    let bad = U.StringSet.diff all (U.StringSet.inter fs te) in
+    if not (U.StringSet.mem "start" all) then warn "missing start";
+    if not (U.StringSet.mem "error" all) then warn "missing error";
+    U.StringSet.iter (fun s -> warn (sprintf "unused state: %s" s)) bad
 
-  let warn_bad_edge msg e vs = match StringSet.elements vs with
+  let warn_bad_edge msg e vs = match U.StringSet.elements vs with
     | [] -> ()
     | vs ->
         fprintf str_formatter "%s->%s: " e.A.edge_source e.A.edge_target;
         fprintf str_formatter "%s: " msg;
-        pp_list ", " pp_s str_formatter vs;
+        U.pp_list ", " U.pp_s str_formatter vs;
         warn (flush_str_formatter ())
 
   let check_linear_patterns p =
     let check_edge e =
       let see (seen, bad) x =
-        if StringSet.mem x seen
-        then (seen, StringSet.add x bad)
-        else (StringSet.add x seen, bad) in
+        if U.StringSet.mem x seen
+        then (seen, U.StringSet.add x bad)
+        else (U.StringSet.add x seen, bad) in
       let ps = A.patterns e in
-      let _, vs = List.fold_left see (StringSet.empty, StringSet.empty) ps in
+      let _, vs = List.fold_left see (U.StringSet.empty, U.StringSet.empty) ps in
       warn_bad_edge "multiple bindings" e vs in
     List.iter check_edge p.A.edges
 
   let bindings =
     let f _ e =
       let ps = A.patterns e in
-      List.fold_left (flip StringSet.add) StringSet.empty ps in
-    y (memo f)
+      List.fold_left (U.flip U.StringSet.add) U.StringSet.empty ps in
+    U.y (U.memo f)
 
   let dump_set s =
     eprintf "@[";
-    pp_list ", " pp_s err_formatter (StringSet.elements s);
+    U.pp_list ", " U.pp_s err_formatter (U.StringSet.elements s);
     eprintf "@]"
 
   (*
@@ -308,41 +308,41 @@ module PropertyChecks = struct
     let outgoing = adjacency_of_edges get_source (fun e->e) p.A.edges in
     let ve = Hashtbl.create 13 in (* cache for edge values *)
     let rec loop vv dv =
-      if StringSet.is_empty dv then vv else begin
+      if U.StringSet.is_empty dv then vv else begin
         let ef e =
           try Hashtbl.find ve e
           with Not_found ->
-            let vf v = try StringMap.find v vv with Not_found -> d in
+            let vf v = try U.StringMap.find v vv with Not_found -> d in
             let r = ef (vf (get_source e)) e in
             Hashtbl.add ve e r; r in
-        let v = StringSet.choose dv in
-        let dv = StringSet.remove v dv in
+        let v = U.StringSet.choose dv in
+        let dv = U.StringSet.remove v dv in
         let w = vf (List.map ef (incoming v)) v in
         let edv =
-          if not (StringMap.mem v vv) || StringMap.find v vv <> w then begin
+          if not (U.StringMap.mem v vv) || U.StringMap.find v vv <> w then begin
             let es = outgoing v in
             List.iter (fun e -> Hashtbl.remove ve e) es;
             List.map get_target es
           end else [] in
-        loop (StringMap.add v w vv) (List.fold_left (flip StringSet.add) dv edv)
+        loop (U.StringMap.add v w vv) (List.fold_left (U.flip U.StringSet.add) dv edv)
       end in
-    let m = loop StringMap.empty (StringSet.singleton "start") in
-    default d StringMap.find m
+    let m = loop U.StringMap.empty (U.StringSet.singleton "start") in
+    default d U.StringMap.find m
 
   let bound_variables p =
     let v_k es_k v = match es_k, v with
-      | _, "start" | [], _ -> StringSet.empty
-      | e::ev, _ -> List.fold_left StringSet.inter e ev in
-    let e_k s_k e = StringSet.union s_k (bindings e) in
-    fixpoint_on_graph p StringSet.empty v_k e_k
+      | _, "start" | [], _ -> U.StringSet.empty
+      | e::ev, _ -> List.fold_left U.StringSet.inter e ev in
+    let e_k s_k e = U.StringSet.union s_k (bindings e) in
+    fixpoint_on_graph p U.StringSet.empty v_k e_k
 
   let check_bound_variables p =
     let bound = bound_variables p in
     let check_edge e =
       let guards = A.guards e in
-      let guards = List.fold_left (flip StringSet.add) StringSet.empty guards in
+      let guards = List.fold_left (U.flip U.StringSet.add) U.StringSet.empty guards in
       let bound_here = bound (get_source e) in
-      warn_bad_edge "possibly unbound" e (StringSet.diff guards bound_here) in
+      warn_bad_edge "possibly unbound" e (U.StringSet.diff guards bound_here) in
     List.iter check_edge p.A.edges
 
   let all p =
