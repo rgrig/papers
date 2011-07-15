1ee63fd343e18a7ef59cd293f81195709a54f7e7
Radu Grigore <radugrigore@gmail.com>
2011-07-15 11:11:42 +0100

    tweaks
    - gitignore sigplanconf.cls
    - better name for class in first example
    - fixed definition of iterated automaton nondeterministic step

--- safety/fig_code/IncorrectIteratorUse.java
+++ safety/fig_code/IncorrectIteratorUse.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Cme {
+public class IncorrectIteratorUse {
   public static void main(String[] args) {
     List<Integer> c = new ArrayList<Integer>();
     c.add(1); c.add(2);
