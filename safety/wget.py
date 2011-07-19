0ce9af90b4a15618aad8a0f04f5cae629dfd10bb
Radu Grigore <radugrigore@gmail.com>
2011-07-19 19:20:05 +0100

    wget.py that works with both python 2 and 3
    didn't test properly last time :(

--- safety/wget.py
+++ safety/wget.py
@@ -1,8 +1,15 @@
 import sys
-import urllib
+if sys.version_info[0] == 3:
+  from urllib.request import urlopen
+else:
+  from urllib import urlopen
 
 if len(sys.argv) != 3:
   print('usage: wget.py url file')
   sys.exit(1)
 
-urllib.urlretrieve(sys.argv[1], sys.argv[2])
+n = urlopen(sys.argv[1])
+l = open(sys.argv[2], 'wb')
+l.write(n.read())
+n.close()
+l.close()
