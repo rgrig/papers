753e7ee33259ce24b29829b51828636d85d738f6
Radu Grigore <radugrigore@gmail.com>
2012-04-14 09:10:18 +0100

    described the pseudocode for the checker

--- safety/Completeness.java
+++ safety/Completeness.java
@@ -6,8 +6,9 @@ public class Completeness {
       Iterator[] a = new Iterator[n];
       for (int i=0; i<n; i++) {
           a[i]=c.iterator();
-          if (r.nextBoolean()) 
-	    while (a[i].hasNext()) a[i].next();	
+          if (r.nextBoolean())
+	    while (a[i].hasNext())
+              a[i].next();
       };
       a[r.nextInt(n)].next();
   };
