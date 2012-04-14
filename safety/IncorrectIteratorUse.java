59d1ca1feb5d5c48c87885cd930fd5beb70c81f8
Radu Grigore <radugrigore@gmail.com>
2012-04-14 11:07:13 +0100

    Some fixe, one big example in the appendix.

--- safety/IncorrectIteratorUse.java
+++ safety/IncorrectIteratorUse.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class IncorrectIteratorUse {
   public static void main(String[] args) {
-    List<Integer> c = new ArrayList<Integer>();
+    List<Integer> c =
+      new ArrayList<Integer>();
     c.add(1); c.add(2);
     Iterator<Integer> i = c.iterator();
     Iterator<Integer> j = c.iterator();
