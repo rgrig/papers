c9ff8e735ff48d513122d12c12e599f3eb8f58c1
Radu Grigore <radugrigore@gmail.com>
2012-07-11 00:24:07 +0100

    typesetting, removed a big figure

--- safety/Taint.java
+++ safety/Taint.java
@@ -1,30 +1,19 @@
 interface Str {
-  void set(int i, char c);
-  char get(int i);
+  void set(int i, char c);  char get(int i);
   int len();
   Itr itr();
 }
-
 interface Itr {
-  char next();
+  boolean hasNext();        char next();
   void set(char c); 
-  boolean hasNext();
 }
-
 class CharArray implements Str {
   char [] data;
-       
-    ....
-    ....      
+  // ...
 }
-
 class Concat implements Str {
   Str one, two;
-  int len;
-
-   .....
-   .....
-
-  public static Concat make(Str one, Str two) { return new Concat(one, two);}
+  public static Concat make(Str one, Str two) { /* ... */ }
+  // ...
 }
 
